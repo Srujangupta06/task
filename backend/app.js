bd5b08f6affe9b2b6904bf20971d0156f3251326
Bhavana <bhavanamuram2001@gmail.com>
2025-07-28 13:52:36 +0530

    employee api added

--- backend/app.js
+++ backend/app.js
@@ -108,7 +108,36 @@ app.get('/api/job-seeker/list', async (req, res) => {
 });
 
 
+// end point for employer 
 
+app.post('/api/employer/registration', async (req,res)=>
+{
+  try{
+    const { company_name, company_email, meeting_link, contact_name, contact_mobile, zip_code, openings, technologies, contact_email} = req.body;
+    //insert the data using parameterized query
+
+    const insertQuery = `INSERT INTO employer (company_name, company_email, meeting_link, contact_name, contact_mobile, zip_code, openings, technologies ,contact_email) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)`
+ 
+    await pool.promise().query(insertQuery, [ company_name, company_email, meeting_link, contact_name, contact_mobile, zip_code, openings, technologies, contact_email]);
+
+    return res.status(200).json({
+      statusCode:200,
+      message: 'employer registration successful'
+      
+    })
+
+  }
+  catch(e){
+    console.error('ERROR JOB SEEKER REGISTRATION:', e.message);
+        return res.status(400).json({
+            statusCode: 400,
+            message: e.message || 'Something went wrong'
+        });
+    
+
+  }
+
+});
 
 // SERVER INITIALIZATION
 const initializeDBAndServer = async () => {
